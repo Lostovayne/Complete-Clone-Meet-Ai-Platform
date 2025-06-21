00652d16fcbe98c5e40bbccd399ebb7c4c680b3c
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-21 00:57:10 -0400

    fix(next.config): ensure type checking and linting are enabled during the build process

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // Enable type checking during the build process
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Enable linting during the build process
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
