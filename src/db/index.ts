0a6ba13470066695596023685ddda0b67b5b6ca0
LostoVayne <moises.barrera.dev@gmail.com>
2025-06-22 21:29:15 -0400

    fix(db): simplify database connection by providing default URL if DATABASE_URL is not defined

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,8 +2,4 @@
 
 import { drizzle } from "drizzle-orm/neon-http";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
-}
-
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/postgres");
