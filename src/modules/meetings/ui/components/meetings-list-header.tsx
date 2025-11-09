5d647d9796d78d3e9e5b685b0b2e502ac8861871
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2025-11-08 23:51:22 -0300

    feat(meetings): enable new meeting dialog in meetings list header

--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -2,16 +2,19 @@
 
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
+import { useState } from "react";
 import NewMeetingDialog from "./new-meeting-dialog";
 
 const MeetingsListHeader = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
   return (
     <>
-      <NewMeetingDialog open={false} onOpenChange={() => {}} />
+      <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen} />
       <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
         <div className="flex items-center justify-between">
           <h5 className="font-medium text-xl">My Meetings</h5>
-          <Button>
+          <Button onClick={() => setIsDialogOpen(true)}>
             <PlusIcon />
             New Meeting
           </Button>
